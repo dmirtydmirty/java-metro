44019d3091f6935658a18746749ccb1a5c627a70
Антонов Дмитрий Игоревич <107511435+dmirtydmirty@users.noreply.github.com>
2024-11-22 01:08:09 +0300

    remove spaces

--- src/Main.java
+++ src/Main.java
@@ -6,25 +6,21 @@ class Main {
         MetroSystem metroSystem = InitMs.initSpbMetro();
 
         Station start = metroSystem.getStationByName("Площадь мужества");
-        Station finish = metroSystem.getStationByName("Парная");
+        Station finish = metroSystem.getStationByName("Спортивная");
 
-        // Создание путешественника
         Traveler traveler1 = new Traveler("Путешественник 1", start);
 
-        // Поиск кратчайшего пути
         List<Station> path = traveler1.findShortestPath(metroSystem, finish);
         metroSystem.setPath(path);
         System.out.println("Кратчайший путь для " + traveler1.name + ":");
         for (Station station : path) {
-            System.out.print(station.name + " ");
+            System.out.print(" -> " +  station.name);
         }
         System.out.println();
 
         MetroMap GUI = new MetroMap();
         GUI.setMs(metroSystem);
         GUI.mainDraw();
-        // Запуск визуализации
-//        MetroVisualization.launch(MetroVisualization.class);
     }
 }
 
